dce1f25522d79a3f0789c4c289c6301db45bff6b
fredev <fredev@satoshilira.io>
2023-06-01 19:26:41 +0200

    chore: remove backup

--- old/packages/ui/src/Box/Box.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import {compose, flexbox} from 'styled-system'
-
-export const Box = styled("div")(
-  compose(
-    flexbox
-  )
-)
--- old/packages/ui/src/Col/Col.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import {Row} from '../Row/Row';
-
-export const Col = styled(Row)`
-  flex-direction: column;
-`
