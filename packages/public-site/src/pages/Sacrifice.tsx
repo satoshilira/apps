db26764720faf99acea7212068e3508c8529c158
fredev <fredev@satoshilira.io>
2023-06-16 15:21:25 +0200

    feat: use deployments library

--- packages/public-site/src/pages/Sacrifice.tsx
+++ packages/public-site/src/pages/Sacrifice.tsx
@@ -1,11 +1,15 @@
 import { Typography } from '../components/Typography';
-import {useSacrifice} from "../hooks";
-import {Col} from "../components/Col";
-import {Countdown} from "../components";
+import {useSacrifice} from '../hooks';
+import {Col} from '../components/Col';
+import {Countdown} from '../components';
+import {useWagmiNetwork} from '../hooks/useChain';
 
 export default function Sacrifice() {
   const {bonus, started, sacrified, sacrificable, end} = useSacrifice()
   console.log('sacrifice', {bonus, started})
+
+  const { chain } = useWagmiNetwork()
+  console.log('chain', chain)
 
   return (
     <Col>
