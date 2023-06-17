df53bd34a129062acb805752a319d37932932a37
fredev <fredev@satoshilira.io>
2023-06-17 22:16:39 +0200

    feat: connect button

---
+++ packages/public-site/src/components/ConnectButton/ConnectButton.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Col } from '../Col';
+import { useWallet } from '../../hooks';
+import { Typography } from '../Typography';
+import metamask from '../../img/metamask-fox.svg';
+
+const StyledButton = styled.button`
+  display: flex;
+  max-width: 280px;
+  align-items: center;
+  background-color: transparent;
+  padding: 8px 10px;
+  border: 1px ${props => props.theme.colors.primary} solid;
+  border-radius: 50px;
+  cursor: pointer;
+
+  & > img {
+    margin-right: 8px;
+  }
+
+  &:active {
+    border: 1px ${props => props.theme.colors.secondary} solid;
+  }
+`
+
+export function ConnectButton() {
+  const { isConnected, connect, disconnect } = useWallet()
+
+  return (
+    <Col style={{ minHeight: '100vh' }}>
+      <StyledButton onClick={() => isConnected ? disconnect() : connect()}>
+        <img src={metamask} width={30} />
+        <Typography color="white" margin={0} flexGrow={1}>{isConnected ? 'Disconnect' : 'Connect'} Metamask</Typography>
+      </StyledButton>
+    </Col>
+  )
+}
