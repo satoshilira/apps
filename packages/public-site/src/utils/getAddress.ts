f7c7f01a00c73aa874070e788832e22830689728
fredev <fredev@satoshilira.io>
2023-07-07 03:23:44 +0200

    feat: get contract address by network

---
+++ packages/public-site/src/utils/getAddress.ts
@@ -0,0 +1,17 @@
+import arbitrumGoerliWbtc from '@satoshi-lira/deployments/arbitrumGoerli/MockWBTC.json'
+import arbitrumGoerliSacrifice from '@satoshi-lira/deployments/arbitrumGoerli/LIRASacrifice.json'
+import { Chain } from 'wagmi'
+
+export function getWbtcAddress(network?: Chain & { unsupported?: boolean | undefined; }) {
+  switch(network) {
+    default:
+      return arbitrumGoerliWbtc.address
+  }
+}
+
+export function getSacrificeAddress(network?: Chain & { unsupported?: boolean | undefined; }) {
+  switch(network) {
+    default:
+      return arbitrumGoerliSacrifice.address
+  }
+}
